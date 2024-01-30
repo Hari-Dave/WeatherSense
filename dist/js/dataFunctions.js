235842e1c8b64454332c4ceed8ee20240832a659
Hari-Dave <davehari2004@gmail.com>
2024-01-31 00:15:02 +0530

    add dataFunction js file

---
+++ dist/js/dataFunctions.js
@@ -0,0 +1,55 @@
+// Use Two different API for Weather Data:
+// 1. Current Weather Data -- from openweathermap.api 
+// Get Lat, Lon, Name, etc. basic data from this API and save that into class object
+// 2. 3-hour Forecast 5 Days -- from openweathermap.api
+// Use that class object and get actual Weather Information from this API 
+
+const WEATHER_API_KEY = "c271640d95c9664272c3954e85c173e3";
+
+export const setLocationObject = (locationObj, coordsObj) => {
+  const { lat, lon, name, unit } = coordsObj; // Deconstructing Object
+  locationObj.setLat(lat);
+  locationObj.setLon(lon);
+  locationObj.setName(name);
+  if (unit) {
+    locationObj.setUnit(unit);
+  }
+};
+
+export const getHomeLocation = () => {
+  return localStorage.getItem("defaultWeatherLocation");
+};
+
+export const getWeatherFromCoords = async (locationObj) => {
+  const lat = locationObj.getLat();
+  const lon = locationObj.getLon();
+  const units = locationObj.getUnit();
+  const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`;
+  try {
+    const weatherStream = await fetch(url);
+    const weatherJson = await weatherStream.json();
+    return weatherJson;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getCoordsFromApi = async (entryText, units) => {
+  const regex = /^\d+$/g;
+  const flag = regex.test(entryText) ? "zip" : "q";
+  const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+  const encodedUrl = encodeURI(url);
+  try {
+    const dataStream = await fetch(encodedUrl);
+    const jsonData = await dataStream.json();
+    return jsonData;
+  } catch (err) {
+    console.err(err.stack);
+  }
+};
+
+export const cleanText = (text) => {
+  const regex = / {2,}/g;
+  const entryText = text.replaceAll(regex, " ").trim();
+  return entryText;
+};
